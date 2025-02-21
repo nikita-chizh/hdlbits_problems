87c277ca5a9883da1e346dfcda37a9f247939612
nikita-chizh <nikita1993chizh@gmail.com>
2025-02-21 15:15:56 +0000

    added test for a add sub

---
+++ fpga_programming_handbook/CH3/add_sub.sv
@@ -0,0 +1,21 @@
+// add_sub.sv
+// ------------------------------------
+// Simple combinational adder subtractor block
+// ------------------------------------
+// Author : Frank Bruno
+// Take in a number of bits, split into two halves and add.
+`timescale 1ns/10ps
+module add_sub
+# (parameter SELECTOR, parameter BITS = 16)
+(input  wire  [BITS-1:0] SW, output logic signed [BITS-1:0] LED);
+
+  logic signed [BITS/2-1:0]       a_in;
+  logic signed [BITS/2-1:0]       b_in;
+
+  always_comb begin
+    {a_in, b_in} = SW;
+    $display("a_in = %d  b_in == %b", a_in, b_in);
+    if (SELECTOR == "ADD") LED = a_in + b_in;
+    else                   LED = a_in - b_in;
+  end
+endmodule
--- fpga_programming_handbook/CH3/add_sub_tb.sv
+++ fpga_programming_handbook/CH3/add_sub_tb.sv
@@ -1,29 +1,32 @@
 `timescale 1ns / 1ps
+`define H 4
 
 module add_sub_tb();
     parameter  SELECTOR_ADD = "ADD";
-    parameter BITS_NUM = 8;
+    parameter BITS_NUM = `H * 2;
+    parameter HALF_BITS = `H;
     logic [BITS_NUM-1: 0] input_sw = 0;
     logic signed [BITS_NUM-1: 0] add_result = 0;
     //
     add_sub #(.SELECTOR(SELECTOR_ADD), .BITS(BITS_NUM)) adder (.SW(input_sw), .LED(add_result));
-    logic [BITS_NUM-1:0] testcases[4:0] = {
-        {{BITS_NUM{1} }, {BITS_NUM{2} } },     // generic
-        {{BITS_NUM{-1}}, {BITS_NUM{15}} },   // check first negative
-        {{BITS_NUM{15}}, {BITS_NUM{-1}} },   // check second negative  
-        {{BITS_NUM{-1}}, {BITS_NUM{-2}} },   // check both negative
-        {{BITS_NUM{15}}, {BITS_NUM{1} } }      // check overflow
+    logic [BITS_NUM-1:0] testcases[0:4] = {
+        {`H'd1,  `H'd2},     // generic
+        {-`H'd4, `H'd7},   // check first negative
+        {`H'd7,  -`H'd1},   // check second negative  
+        {-`H'd1, -`H'd2},   // check both negative
+        {`H'd15,  `H'd15}      // check overflow
     };
+    logic signed [`H-1:0]       a_in;
+    logic signed [`H-1:0]       b_in;
     integer testcase = 0;
     initial begin
     for (testcase = 0; testcase < $size(testcases); ++testcase)
         begin
             input_sw = testcases[testcase];
-            #1;
+            {a_in, b_in} = input_sw;
+            $display("testcase = %d  input_sw_high == %b input_sw_low == %b", testcase, a_in, b_in);
+            #10;
+            $display("add_result == %b add_result_d == %d", add_result, add_result);
         end
-    end
-
-    always @(add_result) begin
-        $display("testcase = %d add_result == %b add_result_d == %d", testcase, add_result, add_result);
     end
 endmodule
