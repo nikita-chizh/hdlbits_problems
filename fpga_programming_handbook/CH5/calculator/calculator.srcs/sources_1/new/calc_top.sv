a9577eee6cbdb097177c1d27efd789695cee78b4
nikita-chizh <nikita1993chizh@gmail.com>
2025-03-09 20:28:33 +0000

    strugling with timing

--- fpga_programming_handbook/CH5/calculator/calculator.srcs/sources_1/new/calc_top.sv
+++ fpga_programming_handbook/CH5/calculator/calculator.srcs/sources_1/new/calc_top.sv
@@ -7,79 +7,61 @@
 `define NUM_SEGMENTS 8
 `endif
 `timescale 1ns/10ps
-module calculator_top
-  #
-  (
-   parameter BITS         = 32,
-   parameter NUM_SEGMENTS = `NUM_SEGMENTS,
-   parameter USE_PLL      = "TRUE"
-   )
-  (
-   input wire                      clk,
-   input wire [15:0]               SW,
-   input wire [4:0]                buttons,
-   input wire                      CPU_RESETN,
+module calculator_top #(parameter BITS = 32, parameter NUM_SEGMENTS = `NUM_SEGMENTS)(
+    input wire                      clk,
+    input wire [15:0]               SW,
+    input wire [4:0]                buttons,
+    input wire                      CPU_RESETN,
 
-   output logic [NUM_SEGMENTS-1:0] anode,
-   output logic [7:0]              cathode
-   );
+    output logic [NUM_SEGMENTS-1:0] anode,
+    output logic [7:0]              cathode);
 
-  import calculator_pkg::*;
+    import calculator_pkg::*;
 
-  logic                               clk_50;
-  logic                               reset;
+    logic clk_50;
+    logic reset;
 
-  generate
-    if (USE_PLL == "TRUE") begin : g_USE_PLL
-      logic int_reset;
-      (* ASYNC_REG = "TRUE" *) logic [1:0] reset_sync = '1;
+    logic int_reset;
+    (* ASYNC_REG = "TRUE" *) logic [1:0] reset_sync = '1; //
 
-      sys_pll u_sys_pll
-        (
-         .clk_in1  (clk),
-         .clk_out1 (clk_50),
-         .locked   (int_reset)
-         );
+    sys_pll u_sys_pll( .clk_in1 (clk), .reset(0), .clk_out1 (clk_50), .locked (int_reset));
 
-      always_ff @(posedge clk_50) begin
+    always_ff @(posedge clk_50) begin
         reset_sync <= {reset_sync[0], ~(int_reset & CPU_RESETN)};
-      end
-      assign reset = reset_sync[1];
-    end else  begin : g_NO_PLL
-      assign clk_50 = clk;
-      assign reset = '0; // No reset necessary unless using external reset or PLL
     end
-  endgenerate
+    assign reset = reset_sync[1];
 
-  logic [31:0]                        accumulator;
-  (* mark_debug = "true" *) 
-  logic [NUM_SEGMENTS-1:0][3:0]       encoded;
-  logic [NUM_SEGMENTS-1:0]            digit_point;
+    logic [31:0]                        accumulator;
+    (* mark_debug = "true" *) 
+    logic [NUM_SEGMENTS-1:0][3:0]       encoded;
+    logic [NUM_SEGMENTS-1:0]            digit_point;
 
-  // Capture button events
-  (* ASYNC_REG = "TRUE" *) logic [2:0] button_sync;
-  logic                               counter_en;
-  logic [7:0]                         counter;
-  logic                               button_down;
-  logic [4:0]                         button_capt;
-  logic [15:0]                        sw_capt;
+    // Capture button events
+    (* ASYNC_REG = "TRUE" *) logic [2:0] button_sync;
+    logic                               counter_en;
+    logic [7:0]                         counter;
+    logic                               button_down;
+    logic [4:0]                         button_capt;
+    logic [15:0]                        sw_capt;
 
-  seven_segment #(.NUM_SEGMENTS (NUM_SEGMENTS), .CLK_PER(20))
-  u_seven_segment(
-     .clk          (clk_50),
-     .reset        (reset),
-     .encoded      (encoded),
-     .digit_point  (digit_point),
-     .anode        (anode),
-     .cathode      (cathode)
-     );
+    seven_segment #(.NUM_SEGMENTS (NUM_SEGMENTS), .CLK_PER(20))
+    u_seven_segment(
+        .clk          (clk_50),
+        .reset        (reset),
+        .encoded      (encoded),
+        .digit_point  (digit_point),
+        .anode        (anode),
+        .cathode      (cathode)
+        );
 
     always @(posedge clk_50) begin
         button_down <= '0;
         button_capt <= '0;
         button_sync <= button_sync << 1 | (|buttons);
-        if (button_sync[2:1] == 2'b01) counter_en <= '1;
-        else if (~button_sync[1])      counter_en <= '0;
+        if (button_sync[2:1] == 2'b01) 
+            counter_en <= '1;
+        else if (~button_sync[1])
+            counter_en <= '0;
 
         if (counter_en) begin
             counter <= counter + 1'b1;
@@ -99,12 +81,13 @@ module calculator_top
     end
 
     calculator_moore#( .BITS(BITS)) u_sm( .clk(clk_50), .reset(reset),
-                       .start(button_down), .operation(button_capt),
-                       .operand(sw_capt), .accum(accumulator));
+                        .start(button_down), .operation(button_capt),
+                        .operand(sw_capt), .accum(accumulator));
 
-  always @(posedge clk_50) begin
-    encoded     <= bin_to_bcd(accumulator);
+    always @(posedge clk_50) begin
+    // encoded     <= bin_to_bcd(accumulator);
+    encoded <= accumulator;
     digit_point <= '1;
-  end
+    end
 
 endmodule // calculator_top
